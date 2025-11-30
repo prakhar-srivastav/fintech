0b52f0991442ee0ead01f142d5629e2628976850
Prakhar Srivastava <prakharsrivastava@Prakhars-MacBook-Pro-2.local>
2025-11-30 22:23:14 +0530

    add changes

---
+++ cicd/workflow.sh
@@ -0,0 +1,28 @@
+
+
+# Load values.yaml into environment variables
+source values.properties
+
+echo "Logging into Docker registry: ${DOCKER_REGISTRY}"
+echo "Using username: ${DOCKER_USERNAME}"
+echo "Using password: ${DOCKER_PASSWORD}"
+
+SERVICE="$1"
+TAG="$2"
+NAMESPACE="$3"
+SERVICE_DIR="../service/$SERVICE/"
+HELM_DIR="../helm/$SERVICE/"
+
+# docker login
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+
+# # go the service directory
+docker build -t ${DOCKER_REGISTRY}/${SERVICE}:${TAG} ${SERVICE_DIR}/
+
+# # push the docker image to docker hub
+docker push ${DOCKER_REGISTRY}/${SERVICE}:${TAG}
+
+# # helm install
+helm upgrade --force --install ${SERVICE} ${HELM_DIR} --values ${SERVICE}/values.yaml --namespace ${NAMESPACE} --create-namespace  --set image.repository=${DOCKER_REGISTRY}/${SERVICE} --set image.tag=${TAG}
+
+
