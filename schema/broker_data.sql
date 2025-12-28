d42778a1aaa327c5ff237610ec9476871eab53c4
Prakhar Srivastava <prakharsrivastava@Prakhars-MacBook-Pro-2.local>
2025-12-28 21:41:19 +0530

    update schema

--- schema/broker_data.sql
+++ schema/broker_data.sql
@@ -1,5 +1,6 @@
 CREATE TABLE broker_data (
     when_added   TIMESTAMP NOT NULL,
+    record_time  TIMESTAMP NOT NULL,
     stocks       VARCHAR(50) NOT NULL,
     exchange     VARCHAR(50) NOT NULL,
     open         DECIMAL(12,4),
@@ -19,3 +20,17 @@ CREATE INDEX idx_broker_data_stocks ON broker_data (stocks);
 
 -- Index for exchange
 CREATE INDEX idx_broker_data_exchange ON broker_data (exchange);
+
+-- Composite Index for stocks and record_time
+CREATE INDEX idx_broker_data_stocks_record_time ON broker_data (stocks, record_time);
+
+-- Composite Index for exchange and record_time
+CREATE INDEX idx_broker_data_exchange_record_time ON broker_data (exchange, record_time);
+
+-- Composite Index for stocks, exchange, and record_time
+CREATE INDEX idx_broker_data_stocks_exchange_record_time ON broker_data (stocks, exchange,
+    record_time);
+
+-- Unique constraint to prevent duplicate entries
+ALTER TABLE broker_data
+ADD CONSTRAINT uq_broker_data_unique_entry UNIQUE (record_time, stocks, exchange, granularity);
